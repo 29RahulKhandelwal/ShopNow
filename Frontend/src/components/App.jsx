445fd582b14d605903d233171a3d65c43cfb378c
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-02-17 19:08:34 +0530

    created data.js for storing product details & added some images

--- Frontend/src/components/App.jsx
+++ Frontend/src/components/App.jsx
@@ -35,7 +35,7 @@ function App(){
                     </div>
                     <div className="card">
                         <a href="product.html">
-                            <img className="medium" src="./img/iphone13.jpg" alt="product" />
+                            <img className="medium" src="./img/adidas-shoes.jpg" alt="product" />
                         </a>
                         <div className="card-body">
                             <a href="product.html">
@@ -54,7 +54,7 @@ function App(){
                     </div>
                     <div className="card">
                         <a href="product.html">
-                            <img className="medium" src="./img/iphone13.jpg" alt="product" />
+                            <img className="medium" src="./img/shirt.jpg" alt="product" />
                         </a>
                         <div className="card-body">
                             <a href="product.html">
@@ -73,7 +73,7 @@ function App(){
                     </div>
                     <div className="card">
                         <a href="product.html">
-                            <img className="medium" src="./img/iphone13.jpg" alt="product" />
+                            <img className="medium" src="./img/Headphone.jpg" alt="product" />
                         </a>
                         <div className="card-body">
                             <a href="product.html">
@@ -92,26 +92,7 @@ function App(){
                     </div>
                     <div className="card">
                         <a href="product.html">
-                            <img className="medium" src="./img/iphone13.jpg" alt="product" />
-                        </a>
-                        <div className="card-body">
-                            <a href="product.html">
-                                <h2>Iphone 13</h2>
-                            </a>
-                            <div className="rating">
-                                <span><i className="fa fa-star"></i></span>
-                                <span><i className="fa fa-star"></i></span>
-                                <span><i className="fa fa-star"></i></span>
-                                <span><i className="fa fa-star-half-o"></i></span>
-                            </div>
-                            <div className="price">
-                                ₹74999
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <a href="product.html">
-                            <img className="medium" src="./img/iphone13.jpg" alt="product" />
+                            <img className="medium" src="./img/AppleMacbook.jpg" alt="product" />
                         </a>
                         <div className="card-body">
                             <a href="product.html">
---
+++ Frontend/src/data.js
@@ -0,0 +1,56 @@
+const data={
+    products:[
+        {
+            _id:'1',
+            name:'Iphone 13',
+            category:'Mobiles',
+            price:74999,
+            brand:'Iphone',
+            rating:4.7,
+            numReviews:10,
+            description:'Iphone 13 latest mobile available in the market.'
+        },
+        {
+            _id:'2',
+            name:'Adidas neo Mens Vs Set Sneakers',
+            category:'Shoes',
+            price:'1756',
+            brand:'Adidas',
+            rating:4.8,
+            numReviews:8,
+            description:'Top Selling Product'
+        },
+        {
+            _id:'3',
+            name:'FINIVO FASHION Men Cotton Casual ShirtIphone 13',
+            category:'Mobiles',
+            price:585,
+            brand:'FINIVO',
+            rating:4.5,
+            numReviews:5,
+            description:'Cotton Shirt'
+        },
+        {
+            _id:'4',
+            name:'JBL Tune 710BT by Harman, 50 Hours Playtime with Quick Charging Wireless Over Ear Headphones with Mic, Dual Pairing, AUX & Voice Assistant Support for Mobile Phones (Blue)',
+            category:'Headphone',
+            price:4999,
+            brand:'JBL',
+            rating:4.4,
+            numReviews:7,
+            description:'Headphone with so many features.'
+        },
+        {
+            _id:'5',
+            name:' Apple MacBook Pro (14-inch/35.97 cm, Apple M1 Pro chip with 8‑core CPU and 14‑core GPU, 16GB RAM, 512GB SSD) - Space Grey',
+            category:'Laptop',
+            price:187990,
+            brand:'Apple',
+            rating:4.8,
+            numReviews:15,
+            description:'Apple Macbook Pro'
+        },
+    ],
+};
+
+export default data;

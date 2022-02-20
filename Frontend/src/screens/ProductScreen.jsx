a499ec23a38b6ffa018db58ef5cc8ac861eb0124
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-02-20 19:12:38 +0530

    updated data.js

--- Frontend/src/screens/ProductScreen.jsx
+++ Frontend/src/screens/ProductScreen.jsx
@@ -1,8 +1,26 @@
 import React from "react";
+import data from "../data";
+import Rating from "../components/Rating";
 
-function ProductScreen(){
+function ProductScreen(props){
+    const product=data.products.find(x=>x._id === props.match.params.id);
+    if(!product){
+        return <div>Product Not Found</div>
+    }
     return (
-        <div>Product Screen</div>
+        <div>
+            <div className="row">
+                <div className="col-2">
+                    <img className="large" src={product.image} alt={product.name}></img>
+                </div>
+                <div className="col-1">
+                    
+                </div>
+                <div className="col-1">
+
+                </div>
+            </div>
+        </div>
     )
 }
 

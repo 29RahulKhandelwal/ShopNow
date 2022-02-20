efa1112ebbc15427eae36be93ff7f764fe8ad7a9
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-02-20 19:18:25 +0530

    showing details of product which matches with the product url id

--- Frontend/src/screens/ProductScreen.jsx
+++ Frontend/src/screens/ProductScreen.jsx
@@ -14,10 +14,21 @@ function ProductScreen(props){
                     <img className="large" src={product.image} alt={product.name}></img>
                 </div>
                 <div className="col-1">
-                    
+                    <ul>
+                        <li>
+                            <h1>{product.name}</h1>
+                        </li>
+                        <li>
+                            <Rating rating={product.rating} numReviews={product.numReviews} />
+                        </li>
+                        <li>
+                            Price : ₹{product.price}
+                        </li>
+                        <li>Description: <p>{product.description}</p></li>
+                    </ul>
                 </div>
                 <div className="col-1">
-
+                    
                 </div>
             </div>
         </div>

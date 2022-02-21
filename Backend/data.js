d623d9c82b2b1515050e8ac66b48387c86dc3698
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-02-21 15:24:42 +0530

    created backend

---
+++ Backend/data.js
@@ -0,0 +1,66 @@
+const data={
+    products:[
+        {
+            _id:'1',
+            name:'Iphone 13',
+            category:'Mobiles',
+            image:'/img/iphone13.jpg',
+            price:74999,
+            countInStock:39,
+            brand:'Iphone',
+            rating:4.2,
+            numReviews:10,
+            description:'Iphone 13 latest mobile available in the market.'
+        },
+        {
+            _id:'2',
+            name:'Adidas neo Mens Vs Set Sneakers',
+            category:'Shoes',
+            image:'/img/adidas-shoes.jpg',
+            price:1756,
+            countInStock:5,
+            brand:'Adidas',
+            rating:4,
+            numReviews:8,
+            description:'Top Selling Product'
+        },
+        {
+            _id:'3',
+            name:'FINIVO FASHION Men Cotton Casual ShirtIphone 13',
+            category:'Mobiles',
+            image:'/img/shirt.jpg',
+            price:585,
+            countInStock:0,
+            brand:'FINIVO',
+            rating:4.5,
+            numReviews:5,
+            description:'Cotton Shirt'
+        },
+        {
+            _id:'4',
+            name:'JBL Tune 710BT by Harman, 50 Hours Playtime with Quick Charging Wireless Over Ear Headphones with Mic, Dual Pairing, AUX & Voice Assistant Support for Mobile Phones (Blue)',
+            category:'Headphone',
+            image:'/img/Headphone.jpg',
+            price:4999,
+            countInStock:28,
+            brand:'JBL',
+            rating:4.5,
+            numReviews:7,
+            description:'Headphone with so many features.'
+        },
+        {
+            _id:'5',
+            name:' Apple MacBook Pro (14-inch/35.97 cm, Apple M1 Pro chip with 8‑core CPU and 14‑core GPU, 16GB RAM, 512GB SSD) - Space Grey',
+            category:'Laptop',
+            image:'/img/AppleMacbook.jpg',
+            price:187990,
+            countInStock:20,
+            brand:'Apple',
+            rating:4.2,
+            numReviews:15,
+            description:'Apple Macbook Pro'
+        },
+    ],
+};
+
+export default data;

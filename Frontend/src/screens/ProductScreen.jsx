74bfabb06625127e06c9447fd377b8c98e663c79
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-02-21 12:56:08 +0530

    product status and add to cart UI

--- Frontend/src/screens/ProductScreen.jsx
+++ Frontend/src/screens/ProductScreen.jsx
@@ -30,7 +30,23 @@ function ProductScreen(props){
                     </ul>
                 </div>
                 <div className="col-1">
-                    
+                    <div className="card card-body">
+                        <ul>
+                            <li>
+                                <div className="row">
+                                    <div>Price</div>
+                                    <div className="price">${product.price}</div>
+                                </div>
+                                <div className="row">
+                                    <div>Status</div>
+                                    <div className="">{product.countInStock > 0 ? (<span className="success">In Stock</span>) : (<span className="error">Unavailable</span>)}</div>
+                                </div>
+                            </li>
+                            <li>
+                                <button className="primary block">Add to Cart</button>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>

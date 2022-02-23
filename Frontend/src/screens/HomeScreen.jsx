82d2bb0fff5ccece61bdf6625c532d218ab09b58
Rahul Khandelwal <rahul290khandelwal@gmail.com>
2022-02-23 14:24:40 +0530

    exported Loading&MessageBox in Homescreen.jsx

--- Frontend/src/screens/HomeScreen.jsx
+++ Frontend/src/screens/HomeScreen.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Product from "../components/Product";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 
 function HomeScreen() {
     const [products, setProducts] = useState([]);
